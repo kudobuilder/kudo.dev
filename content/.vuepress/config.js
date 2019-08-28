1acad887016ca9433b847baf54a0e99809909aa3
Ken Sipe <kensipe@gmail.com>
2019-08-28 17:04:21 -0500

    adding architecture doc

--- content/.vuepress/config.js
+++ content/.vuepress/config.js
@@ -6,6 +6,7 @@ module.exports = {
         sidebar: {
             '/docs/': [
               '',
+              'architecture',
               'cli',
               'comparison',
               'concepts',
