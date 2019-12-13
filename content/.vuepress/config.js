3f19ca5a7a0ea18c58850f2fab365aadf3f547f2
Ken Sipe <kensipe@gmail.com>
2019-12-13 16:33:12 -0500

    Mesos to KUDO blog post (#57)

--- content/.vuepress/config.js
+++ content/.vuepress/config.js
@@ -81,6 +81,7 @@ module.exports = {
                 title: 'Blog',
                 collapsable: false,
                 children: [
+                  'blog-2019-12-06-mesos-to-kudo',
                   'blog-2019-10-kudo-team',
                   'blog-2019-10-hacktoberfest',
                   'announcing-kudo-0.7.2',
