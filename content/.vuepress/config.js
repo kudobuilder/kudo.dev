152ccfb00652a1bdc3446ccfd77c026303f01091
Jan Schlicht <jan@d2iq.com>
2020-01-17 12:57:45 +0100

    Remove 'repository.md'
    Its content is already covered in 'cli.md'.

--- content/.vuepress/config.js
+++ content/.vuepress/config.js
@@ -72,7 +72,6 @@ module.exports = {
                       },
                   ]
               },
-              'repository',
               {
                 title: 'Runbooks',
                 children: [
